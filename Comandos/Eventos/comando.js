3154e5c63e779f2ffc9abe31f6007ed312c276fd
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-10-29 18:50:17 -0300

    Update and rename Comandos/Slash Commands/ping.js to Comandos/Eventos/comando.js

---
+++ Comandos/Eventos/comando.js
@@ -0,0 +1,9 @@
+module.exports = {
+name: "$alwaysExecute",
+code: `
+$reply[$messageID;
+$getServerVar[emojie] » **$username**, o comando \`$message\` não existe, um dos principais erros que pode ter acontecido foi: **erro ortográfico** ou o comando realmente é **inexistente**!
+$onlyIf[$commandinfo[$replaceText[$message[1];$getServerVar[prefix];];name]==;]
+$onlyIf[$stringStartsWith[$message;$getServerVar[prefix]]!=false;]
+`
+}
--- Comandos/Slash Commands/ping.js
+++
@@ -1,6 +0,0 @@
-module.exports = [{
-type: "interactionCommand",
-prototype: "SlashCommand",
-name: "ping",
-code: `Code Here`
-}]
