cf6e1c552ffe1078687e3e23a080dedd1a11a4c7
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-10-29 18:35:32 -0300

    Update conquista.js

--- Comandos/Diversão/conquista.js
+++ Comandos/Diversão/conquista.js
@@ -14,7 +14,7 @@ $reply[$messageID;
 {timestamp:ms}
 ;yes]
 $onlyIf[$message!=;$getServerVar[emojie] » **$username**, informe a mensagem que aparecerá nos stories do Donal J. Trump!]
-$suppressErrors[$getServerVar[emojie] » **$username**, algo deu errado ao executar o comando!]
+$suppressErrors[$getServerVar[emojie] » **$username**, algo deu errado ao tentar executar o comando!]
 $globalCooldown[5s;$getServerVar[emojie] » **$username**, calma ai apressadinho espere **\`[ $replaceText[$replaceText[$getCooldownTime[5s;globalUser;conquista;$authorID];seconds;Segundos];second;Segundo] ]\`** para executar o comando novamente!]
 `
 }
