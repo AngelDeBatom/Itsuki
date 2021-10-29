578adce4f997ae2b2b607864e06adf3a961b4295
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-10-29 18:55:34 -0300

    Update comando.js

--- Comandos/Eventos/comando.js
+++ Comandos/Eventos/comando.js
@@ -2,7 +2,7 @@ module.exports = {
 name: "$alwaysExecute",
 code: `
 $reply[$messageID;
-$getServerVar[emojie] » **$username**, o comando \`$replaceText[$message;$message[1];]\` não existe, um dos principais erros que pode ter acontecido foi: **erro ortográfico** ou o comando realmente é **inexistente**!
+$getServerVar[emojie] » **$username**, o comando \`$replaceText[$message;$message[1];-1]\` não existe, um dos principais erros que pode ter acontecido foi: **erro ortográfico** ou o comando realmente é **inexistente**!
 $onlyIf[$commandinfo[$replaceText[$message[1];$getServerVar[prefix];];name]==;]
 $onlyIf[$stringStartsWith[$message;$getServerVar[prefix]]!=false;]
 $onlyIf[$message[2]!=;]
