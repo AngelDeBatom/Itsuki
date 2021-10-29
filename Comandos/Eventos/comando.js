fe4ed5547011ec47f2b98b8e4e88ef24eb7cbef9
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-10-29 18:56:36 -0300

    Update comando.js

--- Comandos/Eventos/comando.js
+++ Comandos/Eventos/comando.js
@@ -5,6 +5,6 @@ $reply[$messageID;
 $getServerVar[emojie] » **$username**, o comando \`$replaceText[$message;$message[1];-1]\` não existe, um dos principais erros que pode ter acontecido foi: **erro ortográfico** ou o comando realmente é **inexistente**!
 $onlyIf[$commandinfo[$replaceText[$message[1];$getServerVar[prefix];];name]==;]
 $onlyIf[$stringStartsWith[$message;$getServerVar[prefix]]!=false;]
-$onlyIf[$message[2]!=;]
+$onlyIf[$message!=;]
 `
 }
