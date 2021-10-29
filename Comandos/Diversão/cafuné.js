d36776aa9d53eb9f8547603d8a5224134a6b9474
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-10-29 18:17:53 -0300

    Update cafuné.js

--- Comandos/Diversão/cafuné.js
+++ Comandos/Diversão/cafuné.js
@@ -8,11 +8,11 @@ $reply[$messageID;
 {author:🤗 $username fez um cafuné em $username[$mentioned[1]]:$authorAvatar}
 {image:$randomText[https://media.tenor.com/images/c7192cc8ffa738690156fbb9334a8937/tenor.gif;https://67.media.tumblr.com/aadf9ceaee89957b111e844a53dbf806/tumblr_o925gmua611uhpv9wo1_500.gif]}
 {description:}
-{color:F39A63}
+{color:$getServerVar[color]}
 
 ;yes]
-$onlyIf[$mentioned[1]!=;<:errado:895110700500934667> » **$username**, informe a menção do usuário que você deseja fazer cafuné!]
-$suppressErrors[<:errado:895110700500934667> » **$username**, algo deu errado ao executar o comando!]
-$globalCooldown[5s;<:errado:895110700500934667> » **$username**, calma ai apressadinho espere ***\`[ $replaceText[$replaceText[$getCooldownTime[5s;globalUser;cafuné;$authorID];seconds;Segundos];second;Segundo] ]\`*** para executar o comando novamente!]
+$onlyIf[$mentioned[1]!=;$getServerVar[emojie] » **$username**, informe a menção do usuário que você deseja fazer cafuné!]
+$suppressErrors[$getServerVar[emojie] » **$username**, algo deu errado tentar ao executar o comando!]
+$globalCooldown[5s;$getServerVar[emojie] » **$username**, calma ai apressadinho espere \`[ $replaceText[$replaceText[$getCooldownTime[5s;globalUser;cafuné;$authorID];seconds;Segundos];second;Segundo] ]\` para executar o comando novamente!]
 `
 }
