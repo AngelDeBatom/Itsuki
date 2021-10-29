ac743b43dd62a640466beabd749de86df125a5ad
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-10-29 18:39:46 -0300

    Update trump.js

--- Comandos/Diversão/trump.js
+++ Comandos/Diversão/trump.js
@@ -9,9 +9,11 @@ $reply[$messageID;
 {color:F39A63}
 {description:}
 {image:https://api.no-api-key.com/api/v2/trump?message=$replaceText[$message; ;+;-1]}
+{timestamp:ms}
+{footer:Executado por: $userTag:$authorAvatar}
 ;yes]
-$onlyIf[$message!=;<:errado:895110700500934667> » **$username**, informe a mensagem que aparecerá no twitter de Donald Trump!]
-$suppressErrors[<:errado:895110700500934667> » **$username**, algo deu errado ao executar o comando!]
-$globalCooldown[5s;<:errado:895110700500934667> » **$username**, calma ai apressadinho espere ***\`[ $replaceText[$replaceText[$getCooldownTime[5s;globalUser;trump;$authorID];seconds;Segundos];second;Segundo] ]\`*** para executar o comando novamente!]
+$onlyIf[$message!=;$getServerVar[emojie] » **$username**, informe a mensagem que aparecerá no twitter de Donald Trump!]
+$suppressErrors[$getServerVar[emojie] » **$username**, algo deu errado ao tentar executar o comando!]
+$globalCooldown[5s;$getServerVar[emojie] » **$username**, calma ai apressadinho espere **\`[ $replaceText[$replaceText[$getCooldownTime[5s;globalUser;trump;$authorID];seconds;Segundos];second;Segundo] ]\`** para executar o comando novamente!]
 `
 }
