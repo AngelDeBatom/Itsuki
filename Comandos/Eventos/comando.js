b40d5b436ee068363e410323c797eb4ffd878cff
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-10-29 19:22:22 -0300

    Update comando.js

--- Comandos/Eventos/comando.js
+++ Comandos/Eventos/comando.js
@@ -3,7 +3,7 @@ name: "$alwaysExecute",
 code: `
 $reply[$messageID;
 $getServerVar[emojie] » **$username**, o comando \`$message[1]\` não existe, um dos principais erros que pode ter acontecido foi: **erro ortográfico** ou o comando realmente é **inexistente**!
-$onlyIf[$commandinfo[$replaceText[$message[1];$getServerVar[prefix];];name]!=;]
+$onlyIf[$commandinfo[$replaceText[$message[1];$getServerVar[prefix];];name]!=false;]
 $onlyIf[$message[1]!=;]
 $onlyIf[$stringStartsWith[$splitText[1];$getServerVar[prefix]]!=false;]
 $splitText[$message[1];]
