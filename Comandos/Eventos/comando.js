195ec6ff780680563c9e1ad7cbe4f7e6de23cf1f
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-10-29 19:21:18 -0300

    Update comando.js

--- Comandos/Eventos/comando.js
+++ Comandos/Eventos/comando.js
@@ -5,6 +5,7 @@ $reply[$messageID;
 $getServerVar[emojie] » **$username**, o comando \`$message[1]\` não existe, um dos principais erros que pode ter acontecido foi: **erro ortográfico** ou o comando realmente é **inexistente**!
 $onlyIf[$commandinfo[$replaceText[$message[1];$getServerVar[prefix];];name]!=;]
 $onlyIf[$message[1]!=;]
-$onlyIf[$stringStartsWith[$message;$getServerVar[prefix]]!=false;]
+$onlyIf[$stringStartsWith[$splitText[1];$getServerVar[prefix]]!=false;]
+$splitText[$message[1];]
 `
 }
