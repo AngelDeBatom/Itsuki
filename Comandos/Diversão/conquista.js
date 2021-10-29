f3451f2e07a9745dc66c0b856638dd4fc455a451
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-10-29 18:29:59 -0300

    Update conquista.js

--- Comandos/Diversão/conquista.js
+++ Comandos/Diversão/conquista.js
@@ -9,10 +9,12 @@ $reply[$messageID;
 {author:Conquista:$authorAvatar}
 {description:}
 {image:https://minecraftskinstealer.com/achievement/$random[1;39]/Achievement+Get%21/$replaceText[$message;;+;-1]}
-{color:F39A63}
+{color:$getServervar[color]}
+{footer:Executado por: $userTag:$authorAvatar}
+{timestamp:ms}
 ;yes]
-$onlyIf[$message!=;<:errado:895110700500934667> » **$username**, informe a mensagem que aparecerá nos stories do Donal J. Trump!]
-$suppressErrors[<:errado:895110700500934667> » **$username**, algo deu errado ao executar o comando!]
-$globalCooldown[5s;<:errado:895110700500934667> » **$username**, calma ai apressadinho espere ***\`[ $replaceText[$replaceText[$getCooldownTime[5s;globalUser;conquista;$authorID];seconds;Segundos];second;Segundo] ]\`*** para executar o comando novamente!]
+$onlyIf[$message!=;$getServerVar[emojie] » **$username**, informe a mensagem que aparecerá nos stories do Donal J. Trump!]
+$suppressErrors[$getServerVar[emojie] » **$username**, algo deu errado ao executar o comando!]
+$globalCooldown[5s;$getServerVar[emojie] » **$username**, calma ai apressadinho espere **\`[ $replaceText[$replaceText[$getCooldownTime[5s;globalUser;conquista;$authorID];seconds;Segundos];second;Segundo] ]\`** para executar o comando novamente!]
 `
 }
