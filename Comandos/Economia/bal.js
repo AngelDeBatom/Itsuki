2468f026159e7cb8f5428a358e415a3f4f0e62c2
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-10-29 20:33:21 -0300

    Update bal.js

--- Comandos/Economia/bal.js
+++ Comandos/Economia/bal.js
@@ -6,15 +6,16 @@ code: `
 
 $reply[$messageID;{author:Carteira de $username[$findUser[$message]]:$userAvatar[$findUser[$message]]}{description:
 
-> <:grana:895209219287040040> » **TéoCoins** ***\`[ $replaceText[$getGlobalUserVar[téocoins;$findUser[$message]];Infinity;∞] ]\`***, Rank Position ***\`[ $getLeaderboardInfo[téocoins;$findUser[$message];globaluser;top] ]\`***
-> <:paypal:895205813185052702> » **Paypal** ***\`[ $replaceText[$getGlobalUserVar[paypal;$findUser[$message]];Infinity;∞] ]\`***, Rank Position ***\`[ $getLeaderboardInfo[paypal;$findUser[$message];globaluser;top] ]\`***
-> <:gasolina:895413288832221186> » **Gasolina** ***\`[ $getGlobalUserVar[gasolina;$findUser[$message]]/100 ]\`***
-> <:mongodb:895524206291325008> » **Nível** ***\`[ $getGlobalUserVar[lvl;$findUser[$message]] » $sum[$getGlobalUserVar[lvl;$findUser[$message]];1] ( $getGlobalUserVar[xp;$findUser[$message]] XP ) next ( $getGlobalUserVar[rxp;$findUser[$message]] XP ) ]\`***, Rank Position ***\`[ $getLeaderboardInfo[lvl;$findUser[$message];globaluser;top] ]\`***
-> <:pet:901974769728102460> » **Pet** ***\`[ $getGlobalUserVar[pet;$findUser[$message]] ( $getGlobalUserVar[petname;$findUser[$message]] ) ]\`***
+> <:grana:895209219287040040> » **TéoCoins** **\`[ $replaceText[$getGlobalUserVar[téocoins;$findUser[$message]];Infinity;∞] ]\`**, Rank Position **\`[ $getLeaderboardInfo[téocoins;$findUser[$message];globaluser;top] ]\`**
+> <:paypal:895205813185052702> » **Paypal** **\`[ $replaceText[$getGlobalUserVar[paypal;$findUser[$message]];Infinity;∞] ]\`**, Rank Position **\`[ $getLeaderboardInfo[paypal;$findUser[$message];globaluser;top] ]\`**
+> <:gasolina:895413288832221186> » **Gasolina** **\`[ $getGlobalUserVar[gasolina;$findUser[$message]]/100 ]\`**
+> <:mongodb:895524206291325008> » **Nível** ***\`[ $getGlobalUserVar[lvl;$findUser[$message]] » $sum[$getGlobalUserVar[lvl;$findUser[$message]];1] ( $getGlobalUserVar[xp;$findUser[$message]] XP ) next ( $getGlobalUserVar[rxp;$findUser[$message]] XP ) ]\`**, Rank Position **\`[ $getLeaderboardInfo[lvl;$findUser[$message];globaluser;top] ]\`**
+> <:pet:901974769728102460> » **Pet** **\`[ $getGlobalUserVar[pet;$findUser[$message]] ( $getGlobalUserVar[petname;$findUser[$message]] ) ]\`**
+> 💍 » **Casado com** **\`[ $getGlobalUserVar[casadocom;$findUser[$message]] ]\`**
 }{color:F39A63};yes]
 
-$suppressErrors[<:errado:895110700500934667> » **$username**, algo deu errado ao executar o comando!]
-$globalCooldown[5s;<:errado:895110700500934667> » **$username**, calma ai apressadinho espere ***\`[ $replaceText[$replaceText[$getCooldownTime[5s;globalUser;bal;$authorID];seconds;Segundos];second;Segundo] ]\`*** para executar o comando novamente!]
+$suppressErrors[$getServerVar[emojie] » **$username**, algo deu errado ao tentar executar o comando!]
+$globalCooldown[5s;$getServerVar[emojie] » **$username**, calma ai apressadinho espere **\`[ $replaceText[$replaceText[$getCooldownTime[5s;globalUser;bal;$authorID];seconds;Segundos];second;Segundo] ]\`** para executar o comando novamente!]
 `
 }, {
 name: "$alwaysExecute",
