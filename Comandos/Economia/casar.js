d6c871e67618e60ce64a4da0665899d22311fca4
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-10-29 20:12:49 -0300

    Update casar.js

--- Comandos/Economia/casar.js
+++ Comandos/Economia/casar.js
@@ -9,11 +9,11 @@ $reply[$messageID;
 <:zenitsu_love:903708725536231424> <@$authorID> e <@$mentioned[1]> se casaram. Agora os dois receberão **$200** rosquinhas a cada 12hrs!
 ;yes]
 $onlyIf[$mentioned[1]!=;$getServerVar[emojie] » **$username**, informe a menção do usuário que você deseja se casar!]
-$onlyIf[$mentioned[1]!=$authorID;<:zenitsu_love:903708725536231424> » **$username**, você não pode se casar consigo mesmo!]
-$onlyIf[$mentioned[1]!=$isBot[$mentioned[1];<:zenitsu_love:903708725536231424> » **$username**, você não pode se casar com nenhum bot do discord!]
-$onlyIf[$mentioned[1]!=895106609406160978;<:zenitsu_love:903708725536231424> » **$username**, foi mal mas não estou muito afim de me casar com você.]
-$onlyIf[$getServerVar[casado;$mentioned[1]]!=false;]
-$onlyIf[$getServerVar[casado;$authorID]!=false;]
+$onlyIf[$mentioned[1]!=$authorID;$getServerVar[emojie] » **$username**, você não pode se casar consigo mesmo!]
+$onlyIf[$mentioned[1]!=$isBot[$mentioned[1];$getServerVar[emojie] » **$username**, você não pode se casar com nenhum bot do discord!]
+$onlyIf[$mentioned[1]!=895106609406160978;$getServerVar[emojie] » **$username**, foi mal mas não estou muito afim de me casar com você.]
+$onlyIf[$getServerVar[casado;$mentioned[1]]!=false;$getServerVar[emojie] » **$username**, o usuário mencionado já é casado!]
+$onlyIf[$getServerVar[casado;$authorID]!=false;$getServerVar[emojie] » **$username**, você já é casado!]
 `
 }, {
 type: "awaitedCommand",
