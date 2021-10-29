43bbed2b90e499476f5f34586ae1ed3f655a9b00
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-10-29 20:18:15 -0300

    Update casar.js

--- Comandos/Economia/casar.js
+++ Comandos/Economia/casar.js
@@ -4,11 +4,9 @@ aliases: "marry",
 code: `
 $awaitReaction[$mentioned[1];1h;
 <@$mentioned[1]>, você recebeu uma proposta de casamento de <@$authorID>. Reaja com <:zenitsu_love:903708725536231424> para se casar com <@$authorID>!
-;<:zenitsu_love:903708725536231424>;aceitarcasar;] !
-$reply[$messageID;
-<:zenitsu_love:903708725536231424> <@$authorID> e <@$mentioned[1]> se casaram. Agora os dois receberão **$200** rosquinhas a cada 12hrs!
-;yes]
-$onlyIf[$mentioned[1]!=$isBot[$mentioned[1];$getServerVar[emojie] » **$username**, você não pode se casar com nenhum bot do discord!]
+;<:zenitsu_love:903708725536231424>;aceitarcasar;]
+
+$onlyIf[$mentioned[1]!=$isBot[$mentioned[1]];$getServerVar[emojie] » **$username**, você não pode se casar com nenhum bot do discord!]
 $onlyIf[$mentioned[1]!=;$getServerVar[emojie] » **$username**, informe a menção do usuário que você deseja se casar!]
 $onlyIf[$mentioned[1]!=$authorID;$getServerVar[emojie] » **$username**, você não pode se casar consigo mesmo!]
 $onlyIf[$mentioned[1]!=895106609406160978;$getServerVar[emojie] » **$username**, foi mal mas não estou muito afim de me casar com você.]
