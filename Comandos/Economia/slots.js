fcc573609071147c2fae424da1ec8923c3eb5f73
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-10-29 22:03:41 -0300

    Update slots.js

--- Comandos/Economia/slots.js
+++ Comandos/Economia/slots.js
@@ -15,8 +15,8 @@ Você apostou $numberSeparator[$message[1]]  e infelizmente...Perdeu
 $setGlobalUserVar[zenitsucoins;$sub[$getGlobalUserVar[zenitsucoins;$authorID];$message[1]];$authorID]
 $endIf
 $color[$getServerVar[color]]
-$onlyIf[$getGlobalUserVar[Var]>=$message[1];$getServerVar[emojie] » **$username**, você não possui esse valor **ZenitsuCoins**  para apostar!]
-$onlyIf[$message[1]!=;$getServerVar[emojie] » **$username**, é nessecário uma valor para apostar!]
+$onlyIf[$getGlobalUserVar[zenitsucoins]>=$message[1];$getServerVar[emojie] » **$username**, você não possui esse valor **ZenitsuCoins**  para apostar!]
 $onlyIf[$isNumber[$message[1]]==true;$getServerVar[emojie] » **$username**, você so pode apostar números!]
+$onlyIf[$message[1]!=;$getServerVar[emojie] » **$username**, insira a quantia que deseja apostar!]
 $onlyIf[$checkContains[$noMentionMessage;-;--;+;$;&;';";!;?;/;);(;@;#;*;.;,]!=true;$getServerVar[emojie] » **$username**, você não pode inserir nenhum caractere muito menos apostar números negativos!]` 
 }
