b2a65af798fb93311dff257a50ec80e1084b3b51
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-10-29 22:33:56 -0300

    Update cooldown.js

--- Comandos/Economia/cooldown.js
+++ Comandos/Economia/cooldown.js
@@ -6,9 +6,15 @@ code: `
 
 $setVar[comandos;$sum[$getVar[comandos];1]]
 $reply[$messageid;{title:<:futuro:895419380601610250> **Seus Cooldowns**}{color:F39A63}{description:
+<:nada:895112926678097930>**[ Geral ]**
+
+<:nada:895112926678097930>**[ Economia ]**
+
 **Emprego**\n***\`[ $if[$getCooldownTime[5m;globalUser;work;$authorID]==0]
-Use $getServerVar[prefix]work$else$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$getCooldownTime[5m;globalUser;work;$authorID];minutes;Minutos];seconds;Segundos];hours;Horas];and;e];second;Segundo];minute;Minuto];hour;Hora]$endif ]\`***\n**Diário**\n***\`[ $if[$getCooldownTime[12h;globalUser;daily;$authorID]==0]
-Use $getServerVar[prefix]daily$else$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$getCooldownTime[12h;globalUser;daily;$authorID];minutes;Minutos];seconds;Segundos];hours;Horas];and;e];second;Segundo];minute;Minuto];hour;Hora]$endif ]\`***\n**Pesca**\n***\`[ $if[$getCooldownTime[2m;globalUser;fish;$authorID]==0]
+Use $getServerVar[prefix]work$else$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$getCooldownTime[5m;globalUser;work;$authorID];minutes;Minutos];seconds;Segundos];hours;Horas];and;e];second;Segundo];minute;Minuto];hour;Hora]$endif ]\`***
+**Diário**\n***\`[ $if[$getCooldownTime[12h;globalUser;daily;$authorID]==0]
+Use $getServerVar[prefix]daily$else$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$getCooldownTime[12h;globalUser;daily;$authorID];minutes;Minutos];seconds;Segundos];hours;Horas];and;e];second;Segundo];minute;Minuto];hour;Hora]$endif ]\`***
+**Pesca**\n***\`[ $if[$getCooldownTime[2m;globalUser;fish;$authorID]==0]
 Use $getServerVar[prefix]fish$else$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$getCooldownTime[2m;globalUser;fish;$authorID];minutes;Minutos];seconds;Segundos];hours;Horas];and;e];second;Segundo];minute;Minuto];hour;Hora]$endif ]\`***
 }]
 
