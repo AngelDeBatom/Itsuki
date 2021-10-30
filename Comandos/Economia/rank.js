1a82e024b8d785c44de8f3c96bbeff9bf2ef940c
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-10-30 11:47:03 -0300

    Update rank.js

--- Comandos/Economia/rank.js
+++ Comandos/Economia/rank.js
@@ -3,9 +3,9 @@ name: "rank",
 code: `
 $buttonCollector[$get[id];$authorID;1m;rank1,rank2,rank3,rank4,rankinicial;rank1,rank2,rank3,rank4,rankinicial;,,64]
 $let[id;$apiMessage[$channelID;;{author:Rank - Página Inicial:$authorAvatar:}{description:Olá **$username**, seja bem-vindo(a) a página inicial do rank! Para ver os ostentadores de cada rank reaja com alguma categoria dos botões abaixo.}{timestamp:ms}
-{color:F39A63};{actionRow:Level,2,1,rank1:Paypal,2,1,rank2:TéoCoins,2,1,rank3:Xp,2,1,rank4};;yes]]
-$suppressErrors[<:errado:895110700500934667> » **$username**, algo deu errado ao executar o comando!]
-$globalCooldown[5s;<:errado:895110700500934667> » **$username**, calma ai apressadinho espere ***\`[ $replaceText[$replaceText[$getCooldownTime[5s;globalUser;rank;$authorID];seconds;Segundos];second;Segundo] ]\`*** para executar o comando novamente!]
+{color:F39A63};{actionRow:Level,2,1,rank1:Paypal,2,1,rank2:ZenitsuCoins,2,1,rank3:Xp,2,1,rank4};;yes]]
+$suppressErrors[$getServerVar[emojie] » **$username**, algo deu errado ao tentar executar o comando!]
+$globalCooldown[5s;$getServerVar[emojie] » **$username**, calma ai apressadinho espere **\`[ $replaceText[$replaceText[$getCooldownTime[5s;globalUser;rank;$authorID];seconds;Segundos];second;Segundo] ]\`** para executar o comando novamente!]
 `
 }, {
 name: "rank1",
@@ -16,7 +16,7 @@ code: `$interactionReply[;
 $globalUserLeaderboard[lvl;asc]}
 {color:F39A63}
 {timestamp:ms}
-;{actionRow:Página Inicial,2,1,rankinicial:Paypal,2,1,rank2:TéoCoins,2,1,rank3:Xp,2,1,rank4};64;7]`
+;{actionRow:Página Inicial,2,1,rankinicial:Paypal,2,1,rank2:ZenitsuCoins,2,1,rank3:Xp,2,1,rank4};64;7]`
 }, {
 name: "rank2",
 type: "awaitedCommand",
@@ -27,7 +27,7 @@ $interactionReply[;
 $globalUserLeaderboard[paypal;asc;]}
 {color:F39A63}
 {timestamp:ms}
-;{actionRow:Página Inicial,2,1,rankinicial:Level,2,1,rank1:TéoCoins,2,1,rank3:Xp,2,1,rank4};64;7]
+;{actionRow:Página Inicial,2,1,rankinicial:Level,2,1,rank1:ZenitsuCoins,2,1,rank3:Xp,2,1,rank4};64;7]
 `       
 }, {
 name: "rank3",
@@ -51,12 +51,12 @@ $interactionReply[;
 $globalUserLeaderboard[xp;asc]}
 {color:F39A63}
 {timestamp:ms}
-;{actionRow:Página Inicial,2,1,rankinicial:Level,2,1,rank1:Paypal,2,1,rank2:TéoCoins,2,1,rank3};64;7]
+;{actionRow:Página Inicial,2,1,rankinicial:Level,2,1,rank1:Paypal,2,1,rank2:ZenitsuCoins,2,1,rank3};64;7]
 `       
 }, {
 name: "rankinicial",
 type: "awaitedCommand",
 code: `$interactionReply[;
 {author:Rank - Página Inicial:$authorAvatar:}{description:Olá **$username**, seja bem-vindo(a) a página inicial do rank! Para ver os ostentadores de cada rank reaja com alguma categoria dos botões abaixo.}{timestamp:ms}
-{color:F39A63};{actionRow:Level,2,1,rank1:Paypal,2,1,rank2:TéoCoins,2,1,rank3:Xp,2,1,rank4};64;7]`
+{color:F39A63};{actionRow:Level,2,1,rank1:Paypal,2,1,rank2:ZenitsuCoins,2,1,rank3:Xp,2,1,rank4};64;7]`
 }]
