4cf3f81eac6883902e2e5dd2f6251fbbf3554915
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-10-31 13:41:07 -0300

    Create callback.js

---
+++ Utils/callback.js
@@ -0,0 +1,6 @@
+module.exports = (bot) => {
+  bot.readyCommand({
+    channel: "",
+    code:`$log[Fui logado como: $usertag[$clientID]/$clientID\nClient AOI.JS inciado com sucesso.\nClient D.JS iniciado com sucesso.\nFirebase iniciada com sucesso]`
+  })
+}
