294289f54361dc88012c51452de02c4b39700d26
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-10-31 14:04:13 -0300

    Delete callback.js

--- Utils/callback.js
+++
@@ -1,6 +0,0 @@
-module.exports = (bot) => {
-  bot.readyCommand({
-    channel: "",
-    code:`$log[Fui logado como: $usertag[$clientID]/$clientID\nClient AOI.JS inciado com sucesso.\nClient D.JS iniciado com sucesso.\nFirebase iniciada com sucesso]`
-  })
-}
