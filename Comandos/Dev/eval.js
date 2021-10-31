23e79862429f2919d5806ce42d992fd163416448
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-10-31 12:53:02 -0300

    Update eval.js

--- Comandos/Dev/eval.js
+++ Comandos/Dev/eval.js
@@ -1,6 +1,6 @@
 module.exports = {
 name: "eval",
-aliases: ["e"],
+aliases: ["e","ev"],
 code: `
 $eval[$message]
 $onlyforids[$botownerid;542305880054431764;<:errado:895110700500934667> **Ops... |** Somente meu criador tem acesso a esse comando!]
