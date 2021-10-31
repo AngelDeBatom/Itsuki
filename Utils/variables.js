042eddc46fcac0cca99ee08de75f08a238ece7b6
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-10-31 13:52:16 -0300

    Update variables.js

--- Utils/variables.js
+++ Utils/variables.js
@@ -1,29 +1,32 @@
 module.exports = (bot) => {
-  //bot
-  bot.variables({
-    prefix: "t-"
-  })
-  //user
-  bot.variables({
-    usedCommandsUser: "0",
-    usedCommandsUserGlobal: "0",
-    allCommandsUsed: "0",
-    messages: "0",
-    color: "BLUE",
-    afk: "false",
-    afkMTV: "false",
-    registro: "false",
-    blacklist: "false",
-    premium: "false",
-  })
-  //server
-  bot.variables({
-    userToBan: "",
-    userToKick: "",
-    roleToAdd: "",
-    roleToTake: "",
-    antifake: "false",
-    antibot: "false",
-    antilink: "false"
-  })
+bot.variables({
+numblacklist: "0",
+lastblacklist: "Ninguém",
+rblacklist: "",
+blacklist: "false",
+lastunblacklist: "Ninguém",
+embedscolor: "e2f038",
+emojie: "<:zenitsu_crying:903753184709849158>",
+emojin: "<:nada:895112926678097930>",
+pet: "Cachorro",
+petname: "Sem Nome",
+welcomemsg: "Olá {user.mention}, seja bem-vindo(a) ao {guild.name}`({guild.id})`.",
+welcomech: "",
+muted: "",
+comandos: "0",
+welcomechannel: "",
+devs: "838166653039935538;542305880054431764",
+prefix: "z/",
+zenitsucoins: "0",
+paypal: "0",
+gasolina: "100",
+peixes: "0",
+vara: "1",
+lvl: "0",
+xp: "0",
+rxp: "100",
+hotel: "false",
+quartos: "0",
+funcionarios: "0",
+})
 }
