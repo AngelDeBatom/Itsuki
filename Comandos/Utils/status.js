48c47619cb1b0ab250e68023b47083259062fdfc
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-10-31 14:03:51 -0300

    Create status.js

---
+++ Comandos/Utils/status.js
@@ -0,0 +1,26 @@
+module.exports = (bot) => {
+bot.status({
+text: "🍙 Cara, eu amo bolinho de arroz!", 
+type: "STREAMING", 
+status: "idle",
+time: 12
+})
+bot.status({
+text: "Com $numberSeparator[$allMembersCount] amiguinhos! 💛💛💛", 
+type: "PLAYING", 
+status: "idle",
+time: 12
+})
+bot.status({
+text: "Com $serverCount servidores! 🌏🥰", 
+type: "PLAYING", 
+status: "idle",
+time: 12
+})
+bot.status({
+text: "😬 Passando por uma série de reformas...", 
+type: "STREAMING", 
+status: "idle",
+time: 12
+})
+}
