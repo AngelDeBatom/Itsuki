6c0dacb0cef20b34f944bdc857174973fa5b1942
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-02 15:54:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -70,6 +70,38 @@ bot.onInteractionCreate();
 bot.onInteractionCreate();
 bot.onJoined();
 
+bot.variables({
+numblacklist: "0",
+lastblacklist: "Ninguém",
+rblacklist: "",
+blacklist: "false",
+lastunblacklist: "Ninguém",
+embedscolor: "e2f038",
+emojie: "<:zenitsu_crying:903753184709849158>",
+emojin: "<:nada:895112926678097930>",
+pet: "Cachorro",
+petname: "Sem Nome",
+welcomemsg: "Olá {user.mention}, seja bem-vindo(a) ao {guild.name}`({guild.id})`.",
+welcomech: "",
+muted: "",
+comandos: "0",
+welcomechannel: "",
+devs: "838166653039935538;542305880054431764",
+prefix: "z/",
+zenitsucoins: "0",
+paypal: "0",
+gasolina: "100",
+peixes: "0",
+vara: "1",
+lvl: "0",
+xp: "0",
+rxp: "100",
+hotel: "false",
+quartos: "0",
+funcionarios: "0",
+botversion: "Sunshine 1.0.1",
+})
+
 bot.command({
   name: "slashscommands",
   code: `
