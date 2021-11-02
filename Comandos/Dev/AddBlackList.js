aab8b254e2cd17875d05a567f5c40f8f48e9772f
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-01 23:26:51 -0300

    Update AddBlackList.js

--- Comandos/Dev/AddBlackList.js
+++ Comandos/Dev/AddBlackList.js
@@ -2,7 +2,7 @@ module.exports = ({
 name: "add-black-list",
 aliases: "abl",
 code: `$setvar[numblacklist;$sum[$getVar[numblacklist];1]]
-$setvar[lastblacklist;$userTag[$finduser[$message[1]]]]
+$setvar[lastblacklist;$finduser[$message[1]]]
 $setGlobalUserVar[rblacklist;$replacetext[$replacetext[$checkcondition[$messageSlice[1]==];true;Sem razão especificada];false;$messageSlice[1]];$finduser[$message[1]]]
 $setGlobalUserVar[blacklist;true;$finduser[$message[1]]]
 $author[O usuário foi adicionado na blacklist;$useravatar[$finduser[$message[1]]]]
