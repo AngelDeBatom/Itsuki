9c7deb1e8a1e23ae8f04d1d79c14fbe64bf4c4d0
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-02 21:35:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -66,78 +66,25 @@ bot.onMessage({
 })
 
 bot.onInteractionCreate();
-bot.onInteractionCreate();
-bot.onInteractionCreate();
 bot.onJoined();
+bot.onLeave();
 
 bot.variables({
-numblacklist: "0",
-lastblacklist: "Ninguém",
-rblacklist: "",
-blacklist: "false",
-lastunblacklist: "Ninguém",
 embedscolor: "e2f038",
 emojie: "<:zenitsu_crying:903753184709849158>",
 emojin: "<:nada:895112926678097930>",
-pet: "Cachorro",
-petname: "Sem Nome",
-welcomemsg: "Olá {user.mention}, seja bem-vindo(a) ao {guild.name}`({guild.id})`.",
-welcomech: "",
-muted: "",
 comandos: "0",
-welcomechannel: "",
 devs: "838166653039935538;542305880054431764",
-prefix: "z/",
-zenitsucoins: "0",
-paypal: "0",
-gasolina: "100",
-peixes: "0",
-vara: "1",
-lvl: "0",
-xp: "0",
-rxp: "100",
-hotel: "false",
-quartos: "0",
-funcionarios: "0",
+prefix: "s_",
 botversion: "Sunshine 1.0.1",
 })
-
-bot.command({
-  name: "slashscommands",
-  code: `
-$createSlashCommand[$guildID;ping;Veja o meu ping]
-$createSlashCommand[$guildID;8ball;faça uma pergunta para o zenitsu;message:sua pergunta:true:3]
-$createSlashCommand[$guildID;say;Irei reeptir o que você falar;message:sua mensagem:true:3] Slashs commands criado com sucesso!
-$suppressErrors[]
-`
+//Welcome System
+bot.variables({
+welcomemsg: "Olá {user.mention}, seja bem-vindo(a) ao {guild.name}`({guild.id})`.",
+welcomech: "",
+leavemsg: "{user.mention} saiu do servidor, tomará que volto um dia.",
+leavech: ""
 })
-bot.interactionCommand({
- name: "say", 
- code: `$interactionReply[$message]`
- })
-bot.interactionCommand({
- name: "ping", 
- code: `
-$interactionReply[
-📡 Latência da API \`$pingms\`
-🤖 Latência do Bot \`$botPingms\`
-]
-`
- })
-bot.interactionCommand({
- name: "8ball", 
- code: `$interactionReply[
-{author:8ball:$authorAvatar}
-{color:$getServerVar[embedscolor]}
-{description:}
-{field:Pergunta:$message:yes}
-{field:Resposta:$randomText[Com certeza;Concerteza não;Concerteza sim;Claro que não;Claro que sim;Não;Sim;Talvez;Talvez sim;Talvez não]:yes}
-
-]`
- })
-
-
-
 
 
 
