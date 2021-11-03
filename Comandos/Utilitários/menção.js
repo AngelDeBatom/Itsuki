728ac66103e2f9de17004bce9826a014cebcfe65
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-02 22:03:24 -0300

    Update menção.js

--- Comandos/Utilitários/menção.js
+++ Comandos/Utilitários/menção.js
@@ -4,7 +4,6 @@ aliases: ["<@!895106609406160978>"],
 nonPrefixed: true,
 code: `
 $apiMessage[$channelID;
-> 🎃 **Olá ser, vejo que você precisa de ajuda hehe. Meu prefixo é: \`$getServerVar[prefix]\`, obtenha ajuda utilizando: \`$getServerVar[prefix]help/ajuda\`.**\n> 🧙‍♂️ **Tenha um bom Halloween.**
-;;{actionRow:Suporte!,2,5,https\\://discord.gg/3WTCz2vpBs:Vote em Mim!,2,5,https\\://top.gg/bot/895106609406160978};;no]
+Olá **$username**, eu me chamo Soma. Obtenha ajuda utilizando **\`( $getServerVar[prefix]help )\`**, meu prefixo neste servidor é: **\`( $getServerVar[prefix] )\`**.;;{actionRow:Suporte!,2,5,https\\://discord.gg/3WTCz2vpBs};;no]
 `
 }
