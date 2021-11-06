5185ca0cd81bea210cb0223e4996260c724416ad
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-05 22:45:03 -0300

    Update play.js

--- Comandos/Music/play.js
+++ Comandos/Music/play.js
@@ -3,17 +3,17 @@ name: "play",
 aliases: ["p"],
 description: "Tocar uma música",
 code: `
-$if[$queueLength!=1]
+$if[$queueLength<=1]
 **\`( $playSong[$noMentionMessage;5m;yes;yes;<:errado:895110700500934667> » **$username**, não foi possível encontrar a música!] )\`** foi adicionado a queue!
 $suppressErrors[<:errado:895110700500934667> » **$username**, algo deu errado ao tentar executar o comando!]
 $onlyIf[$message!=;<:errado:895110700500934667> » **$username**, eu preciso do nome da música para encontrar um som!]
 $onlyIf[$voiceID!=;<:errado:895110700500934667> » **$username**, você não está em um canal de voz, entre em um!]
-$elseif[$queueLength!=2]
+$elseif[$queueLength>=2]
 **\`( $playSong[$noMentionMessage;5m;yes;yes;<:errado:895110700500934667> » **$username**, não foi possível encontrar a música!] )\`** foi adicionado a queue!
 $suppressErrors[<:errado:895110700500934667> » **$username**, algo deu errado ao tentat executar o comando!]
 $onlyIf[$message!=;<:errado:895110700500934667> » **$username**, eu preciso do nome da música para encontrar um som!]
 $onlyIf[$voiceID!=;<:errado:895110700500934667> » **$username**, você não está em um canal de voz, entre em um!]
-$onlyIf[$voiceID!=$clientID;<:errado:895110700500934667> » **$username**, você precisa estar no mesmo canal de voz que o bot!]
+$onlyIf[$voiceID==$clientID;<:errado:895110700500934667> » **$username**, você precisa estar no mesmo canal de voz que o bot!]
 $endelseif
 $endif
 `
