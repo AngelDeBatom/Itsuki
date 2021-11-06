8b2fb10b4e43a573f79ddb462de6984f7ede3e62
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-05 21:57:21 -0300

    Update play.js

--- Comandos/Music/play.js
+++ Comandos/Music/play.js
@@ -3,10 +3,19 @@ name: "play",
 aliases: ["p"],
 description: "Tocar uma música",
 code: `
-***\`[ $playSong[$noMentionMessage;1d;yes;yes;<:errado:895110700500934667> » **$username**, algo deu errado ao executar o comando, entre em contato com o nosso suporte!] ]\`*** foi adicionado a queue!
-$suppressErrors[<:errado:895110700500934667> » **$username**, algo deu errado ao executar o comando, entre em contato com o nosso suporte!]
+$if[$queueLength==0]
+**\`( $playSong[$noMentionMessage;5m;yes;yes;<:errado:895110700500934667> » **$username**, não foi possível encontrar a música!] )\`** foi adicionado a queue!
+$suppressErrors[<:errado:895110700500934667> » **$username**, algo deu errado ao tentar executar o comando!]
 $onlyIf[$message!=;<:errado:895110700500934667> » **$username**, eu preciso do nome da música para encontrar um som!]
 $onlyIf[$voiceID!=;<:errado:895110700500934667> » **$username**, você não está em um canal de voz, entre em um!]
+$elseif[$queueLength>1]
+**\`( $playSong[$noMentionMessage;5m;yes;yes;<:errado:895110700500934667> » **$username**, não foi possível encontrar a música!] )\`** foi adicionado a queue!
+$suppressErrors[<:errado:895110700500934667> » **$username**, algo deu errado ao tentat executar o comando!]
+$onlyIf[$message!=;<:errado:895110700500934667> » **$username**, eu preciso do nome da música para encontrar um som!]
+$onlyIf[$voiceID!=;<:errado:895110700500934667> » **$username**, você não está em um canal de voz, entre em um!]
+
+$endelseif
+$endif
 `
 }, {
 type: "musicEndCommand",
@@ -18,6 +27,10 @@ A queue de música acabou, estou saindo!
 type: "musicStartCommand",
 channel: "$channelID",      
 code: `
-Tocando agora: ***\`[ $songInfo[title] ]\`***, pedido por: **$username[$songInfo[userID]]**.
+$if[$queueLength==0]
+$elseif[$queueLength>1]
+Tocando agora: **\`( $songInfo[title] )\`**, pedido por: **$username[$songInfo[userID]]**.
+$endelseif
+$endif
 `
 }]
