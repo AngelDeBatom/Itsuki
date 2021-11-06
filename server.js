6c45bdf4aff87bd30d12d2ace5183f04724b2481
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-06 19:54:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ emojie: "<:errado:895110700500934667>",
 emojin: "<:nada:895112926678097930>",
 comandos: "0",
 devs: "838166653039935538;542305880054431764",
-prefix: "//"
+prefix: "s!"
 })
 //Welcome System
 bot.variables({
