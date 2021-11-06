fa9872656ef7bd60391987b715783ba4ae1d1947
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-05 23:01:21 -0300

    Update comando.js

--- Comandos/Eventos/comando.js
+++ Comandos/Eventos/comando.js
@@ -11,7 +11,7 @@ $onlyForIDs[838166653039935538;]
 name: "$alwaysExecute",
 code: `
 $if[$random[10;20]==10]
-$addCmdReactions[😁;😔;😑;🤪;😎;😐:🤨:🤔:😉:🤓]
+$addCmdReactions[😁;😔;😑;🤪;😎;😐;🤨;🤔;😉;🤓]
 $endif
 $suppressErrors[]
 `
