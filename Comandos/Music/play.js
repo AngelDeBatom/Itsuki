882adf1d027cff043f6f41156482f764f49ee6d8
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-05 22:11:09 -0300

    Update play.js

--- Comandos/Music/play.js
+++ Comandos/Music/play.js
@@ -3,12 +3,12 @@ name: "play",
 aliases: ["p"],
 description: "Tocar uma música",
 code: `
-$if[$queueLength<0]
+$if[$queueLength<=0]
 **\`( $playSong[$noMentionMessage;5m;yes;yes;<:errado:895110700500934667> » **$username**, não foi possível encontrar a música!] )\`** foi adicionado a queue!
 $suppressErrors[<:errado:895110700500934667> » **$username**, algo deu errado ao tentar executar o comando!]
 $onlyIf[$message!=;<:errado:895110700500934667> » **$username**, eu preciso do nome da música para encontrar um som!]
 $onlyIf[$voiceID!=;<:errado:895110700500934667> » **$username**, você não está em um canal de voz, entre em um!]
-$elseif[$queueLength>0]
+$elseif[$queueLength>=1]
 **\`( $playSong[$noMentionMessage;5m;yes;yes;<:errado:895110700500934667> » **$username**, não foi possível encontrar a música!] )\`** foi adicionado a queue!
 $suppressErrors[<:errado:895110700500934667> » **$username**, algo deu errado ao tentat executar o comando!]
 $onlyIf[$message!=;<:errado:895110700500934667> » **$username**, eu preciso do nome da música para encontrar um som!]
@@ -27,8 +27,8 @@ A queue de música acabou, estou saindo!
 type: "musicStartCommand",
 channel: "$channelID",      
 code: `
-$if[$queueLength<0]
-$elseif[$queueLength>0]
+$if[$queueLength<=0]
+$elseif[$queueLength>=1]
 Tocando agora: **\`( $songInfo[title] )\`**, pedido por: **$username[$songInfo[userID]]**.
 $endelseif
 $endif
