f0312a2bba229407c001b751ecfcaabbfbbc5755
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-07 19:07:43 -0300

    Update status.js

--- Utils/status.js
+++ Utils/status.js
@@ -1,30 +1,30 @@
 module.exports = (bot) => {
 bot.status({
-text: "🧠 | Working the mind | [$shardID]",
+text: "🧠 | Working the mind | [1]",
 type: "PLAYING",
 status: "online",
 time: 12
 })
 bot.status({
-text: "📦 | Working on version 1.0.2 | [$shardID]", 
+text: "📦 | Working on version 1.0.2 | [2]", 
 type: "STREAMING", 
 status: "online",
 time: 12
 })
 bot.status({
-text: "🍮 | I love pudding | [$shardID]", 
+text: "🍮 | I love pudding | [3]", 
 type: "PLAYING", 
 status: "online",
 time: 12
 })
 bot.status({
-text: "🪐 | A simple bot ready to moderate and entertain your server | [$shardID]", 
+text: "🪐 | A simple bot ready to moderate and entertain your server | [4]", 
 type: "PLAYING", 
 status: "online",
 time: 12
 })
 bot.status({
-text: "💔 | Unavailable Sharding | [$shardID]", 
+text: "💔 | Unavailable Sharding | [5]", 
 type: "STREAMING", 
 status: "online",
 time: 12
