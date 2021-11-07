732ec84c1dec5a900161db1e069274dd153ed247
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-07 20:51:29 -0300

    Update setmute.js

--- Comandos/Moderação/setmute.js
+++ Comandos/Moderação/setmute.js
@@ -12,6 +12,6 @@ $onlyIf[$roleExists[$findRole[$message[1]]]==true;$getServerVar[emojie] **| $use
 $onlyIf[$message[1]!=;$getServerVar[emojie] **| $username**, Incorrect usage mode try \`$getServerVar[prefix]set-mute-role <role | roleID>\`]
 $onlyPerms[manageroles;$getServerVar[emojie] **| $username**, Missing \`Manage Roles\` permission!]
 $onlyBotPerms[manageroles;$getServerVar[emojie] **| $username**, I nedd \`Manage Roles\` permission!]
-$globalCooldown[5s;$getServerVar[emojie] **| $username**, Wait \`$getCooldownTime[5s;globalUser;botinfo;$authorID]\` to use the command again!]
+$globalCooldown[5s;$getServerVar[emojie] **| $username**, Wait \`$getCooldownTime[5s;globalUser;set-mute-role;$authorID]\` to use the command again!]
 ` 
 }
