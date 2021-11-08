ded763bd987350a2e3592cdbffba9f7d68bb6fa3
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-07 22:51:29 -0300

    Update eval.js

--- Comandos/Dev/eval.js
+++ Comandos/Dev/eval.js
@@ -1,22 +1,8 @@
 module.exports = {
 name: "eval",
 aliases: ["e","ev"],
-error: "$editMessage[$message[1];$userTag had a problem: $error]",
 code: `
-$color[$getServerVar[embedscolor]]
-$title[Eval]
-$description[
-]
-$addField[Output;
-\`\`\`js
 $eval[$message]
-\`\`\`
-]
-$addField[Input;
-\`\`\`js
-$message
-\`\`\`
-]
 $onlyIf[$message!=;]
 $onlyForIDs[$botownerid;]
 `
