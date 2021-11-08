200bebb181a5850a195aaeea78074d78d6a8f93d
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-07 21:06:08 -0300

    Update setprefix.js

--- Comandos/Moderação/setprefix.js
+++ Comandos/Moderação/setprefix.js
@@ -2,15 +2,12 @@ module.exports = {
 name: "setprefix",
 description: "Altere o prefixo do bot!",
 code: `
-$reply[$messageID;
-{author:Set Prefix:$authorAvatar}
-{description:**[$message[1]]** foi alterado na minha variavel, como novo prefixo com sucesso.}
-{color:F39A63}
-;yes]
+$title[Set prefix]
+$description[**[$message[1]]** was defined in my database as a new prefix successfully!.]
+$color[$getServerVar[embedscolor]]
 $setServerVar[prefix;$message[1]]
-$onlyIf[$message[1]!=;<:errado:895110700500934667> » **$username**, insira o meu novo prefixo!]
-$onlyIf[$charCount[$message[1]]<=4;<:errado:895110700500934667> » **$username**, o prefixo não pode ter mais de 4 caracteres.]
-$onlyPerms[manageserver;<:errado:895110700500934667> » **$username**, você precisa da permissão \`Gerenciar Servidor\` para utilizar esse comando!]
-$globalCooldown[5s;<:errado:895110700500934667> » **$username**, calma ai apressadinho espere ***\`[ $replaceText[$replaceText[$getCooldownTime[5s;globalUser;setprefix;$authorID];seconds;Segundos];second;Segundo] ]\`*** para executar o comando novamente!]
+$onlyIf[$message[1]!=;$getServerVar[emojie] **| $username**, Incorrect usage mode try \`$getServerVar[prefix]setprefix < new prefix >\`]
+$onlyIf[$charCount[$message[1]]<=4;$getServerVar[emojie] **| $username**, The prefix cannot contain more than 4 characters!]
+$onlyPerms[manageserver;$getServerVar[emojie] **| $username**, Missing \`Manage Server\` permission!]
 `
 }
