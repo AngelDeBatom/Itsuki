5cb721e5548775864c26ba072a517786087475ce
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-07 21:07:59 -0300

    Update setprefix.js

--- Comandos/Moderação/setprefix.js
+++ Comandos/Moderação/setprefix.js
@@ -1,5 +1,5 @@
 module.exports = {
-name: "setprefix",
+name: "set-prefix",
 description: "Altere o prefixo do bot!",
 code: `
 $title[Set prefix]
@@ -9,5 +9,6 @@ $setServerVar[prefix;$message[1]]
 $onlyIf[$message[1]!=;$getServerVar[emojie] **| $username**, Incorrect usage mode try \`$getServerVar[prefix]setprefix < new prefix >\`]
 $onlyIf[$charCount[$message[1]]<=4;$getServerVar[emojie] **| $username**, The prefix cannot contain more than 4 characters!]
 $onlyPerms[manageserver;$getServerVar[emojie] **| $username**, Missing \`Manage Server\` permission!]
-`
+$globalCooldown[5s;$getServerVar[emojie] **| $username**, Wait \`$getCooldownTime[5s;globalUser;set-prefix;$authorID]\` to use the command again!]
+` 
 }
