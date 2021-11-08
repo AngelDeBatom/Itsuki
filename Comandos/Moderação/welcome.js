a2a1d2aad8254fc8a8a4374c17b187f573d5c4bf
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-07 22:05:00 -0300

    Update welcome.js

--- Comandos/Moderação/welcome.js
+++ Comandos/Moderação/welcome.js
@@ -2,38 +2,39 @@ module.exports = [{
 name: "set-wchannel",
 description: "Definir um canal de welcome!",
 code: `
-$setServerVar[welcomech;$mentionedChannels[1]]
-$channelSendMessage[$mentionedChannels[1];<:correto:895110679495839785> » Esse canal foi definido como canal de boas-vindas por: **$username**
-$reply[$messageID;
-<:correto:895110679495839785> » **$username**, o canal de welcome foi definido para, <#$mentionedChannels[1]>
-;yes]
-$onlyIf[$channelExists[$mentioned[1]]!=true;<:errado:895110700500934667> » **$username**, não foi possível encontrar a menção do canal informado!]
-$onlyIf[$mentionedChannels[1]!=;<:errado:895110700500934667> » **$username**, você precisa informar a menção do canal!]
-$onlyPerms[managechannels;<:errado:895110700500934667> » **$username**, você precisa da permissão \`Gerenciar Canais\` para utilizar esse comando!]
-$globalCooldown[5s;<:errado:895110700500934667> » **$username**, calma ai apressadinho espere ***\`[ $replaceText[$replaceText[$getCooldownTime[5s;globalUser;lock;$authorID];seconds;Segundos];second;Segundo] ]\`*** para executar o comando novamente!]
-`
+$setServerVar[welcomech;$findChannel[$message[1]]]
+$channelSendMessage[$findChannel[$message[1]];This channel has been set as a welcome channel, anyone who joins the server I will be letting you know!\nDefined By **$userTag**]
+$title[Welcome Channel]
+$color[$getServerVar[embedscolor]]
+$addTimestamp
+$description[**$username**, <#$findChannel[$message[1]]> has been set as welcome channel]
+$onlyIf[$channelExists[$findChannel[$message[1]]]!=true;$getServerVar[emojie] **| $username**, this channel does not exist!]
+$onlyIf[$mentionedChannels[1]!=;$getServerVar[emojie] **| $username**, Incorrect usage mode try \`$getServerVar[prefix]set-wchannel <channel | channelID>\`]
+$onlyPerms[managechannels;$getServerVar[emojie] **| $username**, Missing \`Manage Channels\` permission!]
+$globalCooldown[5s;$getServerVar[emojie] **| $username**, Wait \`$getCooldownTime[5s;globalUser;set-wchannel;$authorID]\` to use the command again!]
+$suppressErrors[] `
 }, {
 name: "set-wmsg",
 description: "Alterar a mensagem de welcome!",
 code: `
+<@$authorID>
 $setServerVar[welcomemsg;$message]
-$reply[$messageID;
-{timestamp:ms}
-{color:$getServerVar[embedscolor]}
-{author:Set Welcome Message:$authorAvatar}
-{description:
-<:correto:895110679495839785> » **$username**, a mensagem de welcome foi alterada para:\n
+$addTimestamp
+$color[$getServerVar[embedscolor]]
+$title[Welcome Message]
+$description[
+**$username**, The welcome message has been changed to\n
 \`\`\`
 $replaceText[$message;\`;]
 \`\`\`
-}
-;yes]
+]
 $onlyIf[$message!=;
+<@$authorID>
 {timestamp:ms}
 {color:$getServerVar[embedscolor]}
-{author:Set Welcome Message:$authorAvatar}
+{title:Welcome Message}
 {description:
-<:errado:895110700500934667> » **$username**, vejo que esta com dificuldade para alterar a mensagem de welcome, veja minhas variáveis disponíveis de welcome.
+**$username**, I see that it is difficult to change the welcome message, see my available welcome variables.
 \`\`\`
 {user.tag\\} - $userTag
 {user.name\\} - $username
@@ -42,17 +43,16 @@ $onlyIf[$message!=;
 {guild.members\\} - $membersCount
 {guild.id\\} - $guildID
 \`\`\`
-Mensagem atual de welcome:
+Current welcome message:
 \`\`\`
 $replaceText[$getServerVar[welcomemsg];\`;]
 \`\`\`
 }
 ]
-$onlyPerms[managechannels;<:errado:895110700500934667> » **$username**, você precisa da permissão \`Gerenciar Canais\` para utilizar esse comando!]
-$globalCooldown[5s;<:errado:895110700500934667> » **$username**, calma ai apressadinho espere **\`( $replaceText[$replaceText[$getCooldownTime[5s;globalUser;setwelcomemsg;$authorID];seconds;Segundos];second;Segundo] )\`** para executar o comando novamente!]
-`
+$onlyPerms[managechannels;$getServerVar[emojie] **| $username**, Missing \`Manage Channels\` permission!]
+$globalCooldown[5s;$getServerVar[emojie] **| $username**, Wait \`$getCooldownTime[5s;globalUser;set-wmsg;$authorID]\` to use the command again!]  `
 }, {
-name: "welcomestatus",
+name: "welcome-status",
 code: `
 <@$authorID>
 $title[Welcome Status]
@@ -60,13 +60,13 @@ $color[$getServerVar[embedscolor]]
 $description[
 \`\`\`
 Channel: $getserverVar[welcomech]
-Message: $replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$getServerVar[welcomemsg];{usertag};$usertag];{user.name};$username];{user.mention};<@$authorID>];{guild.name};$serverName];{guild.members};$memberCount];{guild.id};$guildid]
+Message: $replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$getServerVar[welcomemsg];{usertag};$usertag];{user.name};$username];{user.mention};<@$authorID>];{guild.name};$serverName];{guild.members};$memberCount];{guild.id};$guildid];\`;]
 Auto-role: $getServerVar[autorole]
 \`\`\`
 ]
 $addTimestamp
-$onlyPerms[managechannels;<:errado:895110700500934667> » **$username**, você precisa da permissão \`Gerenciar Canais\` para utilizar esse comando!]
-$globalCooldown[5s;$getServerVar[emojie] **| $username**, Wait \`$getCooldownTime[5s;globalUser;set-auto-role;$authorID]\` to use the command again!]  `
+$onlyPerms[managechannels;$getServerVar[emojie] **| $username**, Missing \`Manage Channels\` permission!]
+$globalCooldown[5s;$getServerVar[emojie] **| $username**, Wait \`$getCooldownTime[5s;globalUser;welcome-status;$authorID]\` to use the command again!]  `
 }, {
  name: "set-auto-role",
  code: `
@@ -75,7 +75,7 @@ $title[Auto Role]
  $description[**$username**, <@&$findRole[$message[1]]> was defined as the role of welcome!
  $color[$getServerVar[embedscolor]] 
 $addTimestamp
- $setServerVar[autorole;$mentionedRoles[1]]
+ $setServerVar[autorole;$findRole[$message[1]]]
 $onlyIf[$roleExists[$findRole[$message[1]]]==true;$getServerVar[emojie] **| $username**, this role does not exist!]
 $onlyIf[$message[1]!=;$getServerVar[emojie] **| $username**, Incorrect usage mode try \`$getServerVar[prefix]set-auto-role <role | roleID>\`]
 $onlyPerms[manageroles;$getServerVar[emojie] **| $username**, Missing \`Manage Roles\` permission!]
