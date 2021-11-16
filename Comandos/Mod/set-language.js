f0bf21e9cf55e7da6a8589be48563e2ad2598da0
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-16 20:55:37 -0300

    Update set-language.js

--- Comandos/Mod/set-language.js
+++ Comandos/Mod/set-language.js
@@ -4,9 +4,13 @@ code: `
 $if[$message==]
 $getServerVar[setlanguague]
 $elseif[$message==pt]
+$setServerVar[setlanguague;$getServerVar[emojie] <@$authorID>, diga o idioma que direi "pt" para português e "en" para inglês.]
+$setServerVar[botlanguague;🇧🇷 Português de Brasil]
 $getServerVar[emojic] <@$authorID>, agora eu falo português.
 $endelseif
 $elseif[$message==en]
+$resetServerVar[setlanguague;$guildID]
+$resetServerVar[botlanguague;$guildID]
 $getServerVar[emojic] <@$authorID>, now i speak english.
 $endelseif
 $endif
