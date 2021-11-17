22135ca3d9673d074b2fad4694db71ad10b81e6b
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-16 21:57:19 -0300

    Update botinfo.js

--- Comandos/Utilitários/botinfo.js
+++ Comandos/Utilitários/botinfo.js
@@ -10,19 +10,16 @@ $apiMessage[$channelID;<@$authorID>;
 Hi **$username**, my name is Rimuru Tempest, I'm 3 years old and I'm a simple bot created in order to have fun and moderate your server.
 
 I've been living in this world since October 5, 2021 to ensure fun and moderation on your server. :D
- **Information**
-> 👑 Creator \`$userTag[$botownerid]\`
-> 📌 Prefix \`Server: $getServerVar[prefix] || Pattern: r! \`
-> 👥 Total users \`$allMembersCount\`
-> 💙 Total servers \`$serverCount\`
-> 🌍 Language \`$getServerVar[botlanguage]\`
- **Hosting**
-> ☁️ Database \`FireBase \`
-> ⚡ Host \`Heroku\`
- **Statistics**
-> ⏰ Uptime \`$uptime\`
-> 🛰️ Ping \`$pingms\`
-> 📆 Creation date \`October 5, 2021 (5/10/2021)\`
+ **$getServerVar[botinfoinformation]**
+> 👑 $getServerVar[botinfocreator] \`$userTag[$botownerid]\`
+> 📌 $getServerVar[botinfoprefix] \`$getServerVar[botinfoprefixa] $getServerVar[prefix] || $getServerVar[botinfoprefixb] d. \`
+> 👥 $getServerVar[botinfousers]\`$allMembersCount\`
+> 💙 $getServerVar[botinfoguilds] \`$serverCount\`
+> 🌍 $getServerVar[botinfolanguage] \`$getServerVar[botlanguage]\`
+ **$getServerVar[botinfostatistics]**
+> ⏰ $getServerVar[botinfouptime] \`$uptime\`
+> 🛰️ $getServerVar[botinfoping] \`$pingms\`
+> 📆 $getServerVar[botinfocreationdate] \`$getServerVar[botinfocreationdatea]\`
 }
 ;{actionRow:Invite me,2,5,https\\://discord.com/api/oauth2/authorize?client_id=895106609406160978&permissions=8&scope=bot:Support,2,5,https\\://discord.gg/3WTCz2vpBs};;no]
 
