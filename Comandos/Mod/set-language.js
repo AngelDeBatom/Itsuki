81c4627ccc2623b4f71fc4832c7d1a60b5ed3710
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-16 21:11:33 -0300

    Update set-language.js

--- Comandos/Mod/set-language.js
+++ Comandos/Mod/set-language.js
@@ -2,9 +2,9 @@ module.exports = {
 name: "language",
 code: `
 $if[$message==]
-$replaceText[$replaceText[$getServerVar[setlanguage];<@$authorID>;<@$authorID>];$getServerVar[emojie];$getServerVar[emojie]]
+$getServerVar[emojie] <@$authorID>, $getServerVar[setlanguage]
 $elseif[$message==pt]
-$setServerVar[setlanguage;$getServerVar[emojie] <@$authorID>, diga o idioma que direi "pt" para português e "en" para inglês.]
+$setServerVar[setlanguage;diga o idioma que direi "pt" para português e "en" para inglês.]
 $setServerVar[botlanguage;🇧🇷 Português de Brasil]
 $getServerVar[emojic] <@$authorID>, agora eu falo português.
 $endelseif
