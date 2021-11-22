a2bb60e400e9d5ea832620240d872b18e118b09f
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-21 22:23:43 -0300

    Update skip.js

--- Comandos/Music/skip.js
+++ Comandos/Music/skip.js
@@ -4,9 +4,8 @@ aliases: ["s"],
 description: "Skipar uma música!",
 code: `
 $skipSong
-$reply[$messageID;⏩ Skipado!;yes]
-$onlyIf[$queueLength>1;<:errado:895110700500934667> » **$username**, a queue precisa conter mais de ***\`[ 2 ]\`*** sons para dar skip!]
-$onlyIf[$voiceID!=$clientID;<:errado:895110700500934667> » **$username**, você precisa estar no mesmo canal de voz que o bot!]	
-$onlyIf[$queueLength!=0;<:errado:895110700500934667> » **$username**, não possui nenhuma música na queue!]
+$reply[$messageID;Skipado!;yes]
+$onlyIf[$voiceID!=$clientID;<:incorrect:909966535769092156> <@$authorID>, você precisa estar no mesmo canal de voz que o bot!]	
+$onlyIf[$queueLength!=0;<:incorrect:909966535769092156> <@$authorID>, não possui nenhuma música na queue!]
 `
 }
