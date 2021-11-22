13c92f04feff888fc50da7c7b418c9348afd4add
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-21 22:24:47 -0300

    Update clearqueue.js

--- Comandos/Music/clearqueue.js
+++ Comandos/Music/clearqueue.js
@@ -7,5 +7,6 @@ $stopSong
 $reply[$messageID;<:correto:895110679495839785> » **$username**, a queue foi limpa!;yes]
 $onlyIf[$voiceID!=$clientID;<:errado:895110700500934667> » **$username**, você precisa estar no mesmo canal de voz que o bot!]	
 $onlyIf[$queueLength!=0;<:errado:895110700500934667> » **$username**, não possui nenhuma música na queue!]
+$onlyForIDs[$botownerid;]
 `
 }
