34f49bdb1a3193c903ffeaca4495b580c3747387
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-21 22:24:09 -0300

    Update stop.js

--- Comandos/Music/stop.js
+++ Comandos/Music/stop.js
@@ -4,8 +4,8 @@ aliases: ["st"],
 description: "Parar de tocar uma música!",
 code: `
 $stopSong
-$reply[$messageID;⏹️ Parado!;yes]
-$onlyIf[$voiceID!=$clientID;<:errado:895110700500934667> » **$username**, você precisa estar no mesmo canal de voz que o bot!]	
-$onlyIf[$queueLength!=0;<:errado:895110700500934667> » **$username**, não possui nenhuma música na queue!]
+$reply[$messageID;Parado!;yes]
+$onlyIf[$voiceID!=$clientID;<:incorrect:909966535769092156> <@$authorID>, você precisa estar no mesmo canal de voz que o bot!]	
+$onlyIf[$queueLength!=0;<:incorrect:909966535769092156> <@$authorID>, não possui nenhuma música na queue!]
 `
 }
