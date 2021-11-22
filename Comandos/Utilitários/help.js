3099d90384c1f56d3d1c2cc2512794ed1b578441
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-21 22:36:59 -0300

    Update help.js

--- Comandos/Utilitários/help.js
+++ Comandos/Utilitários/help.js
@@ -6,26 +6,15 @@ code: `
 <@$authorID>
 $title[Help Panel]
 $description[
-Any errors found whether spelling or in the command itself report using \`$getServerVar[prefix]bug <bug>\`
-]
-$addField[🛠️ Administration;
-**clear \`|\` ban \`|\` unban \`|\` lock \`|\` unlock \`|\` tempmute**
-]
-$addField[🔧 Configurable;
-**set-prefix \`|\` set-mute-role \`|\` temp-mute \`|\` set-wchannel \`|\` set-wmsg \`|\` s-lchannel \`|\` set-lmsg \`|\` set-autorole**
 ]
 $addField[🧠 General;
-**invite \`|\` ping \`|\` botinfo \`|\` userinfo**
+**invite \`|\` ping \`|\` botinfo \`|\`afk**
 ]
 $addField[🎶 Music;
 **play \`|\` skip \`|\` queue \`|\` loop \`|\` pause \`|\` stop \`|\` resume \`|\` disconnect**
 ]
-$addField[🍕 Fun;
-**hug \`|\` cafuné \`|\` conquista \`|\` happy \`|\` trump \`|\` kiss**
-]
-$footer[Server hosted on shard #$shardID]
+$footer[Servidor hospedado no fragmento #$shardID]
 $addTimestamp
-$color[$getServerVar[embedscolor]]
-$globalCooldown[5s;$getServerVar[emojie] **| $username**, Wait \`$getCooldownTime[5s;globalUser;help;$authorID]\` to use the command again!]
+$color[FFFFFF]
 `
 }
