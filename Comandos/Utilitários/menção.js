573453449cdb6df7c98aba458fc8e80ce101744b
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-21 22:32:42 -0300

    Update menção.js

--- Comandos/Utilitários/menção.js
+++ Comandos/Utilitários/menção.js
@@ -3,6 +3,6 @@ name: "<@895106609406160978>",
 aliases: ["<@!895106609406160978>"],
 nonPrefixed: true,
 code: `
-Hi **$username**, my name is Rimuru Tempest. Get help using \`$getServerVar[prefix]help\`, by the way my prefix on this server is \`$getServerVar[prefix]\`.
+Olá <@$authorID>, meu nome é **Itsuki**. Obtenha ajuda usando **$getServerVar[prefix]help**, a propósito, meu prefixo neste servidor é **$getServerVar[prefix]**.
 `
 }
