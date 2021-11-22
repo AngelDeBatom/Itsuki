c301a4b3347f7becc7adaf7ebdf81195b1f4605e
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-21 22:34:30 -0300

    Update invite.js

--- Comandos/Utilitários/invite.js
+++ Comandos/Utilitários/invite.js
@@ -1,9 +1,6 @@
 module.exports = {
 name: "invite",
 code: `
-$apiMessage[$channelID;<@$authorID>
-Oh I see you liked me right, hahaha. How about inviting me to your server, I hope to help you by having fun and moderating your server, any questions or errors found please contact my support server!;
-;{actionRow:Invite me,2,5,https\\://discord.com/api/oauth2/authorize?client_id=895106609406160978&permissions=8&scope=bot:Support,2,5,https\\://discord.gg/3WTCz2vpBs};;no]
-$globalCooldown[5s;$getServerVar[emojie] **| $username**, Wait \`$getCooldownTime[5s;globalUser;botinfo;$authorID]\` to use the command again!]
+$apiMessage[$channelID;<@$authorID>, ah eu vejo que você gostou de mim né, hahaha. Que tal me convidar para o seu servidor, espero ajudá-lo se divertindo o seu servidor, qualquer dúvida ou erro encontrado entre no meu servidor de suporte!;;{actionRow:Convide-me,2,5,https\\://discord.com/api/oauth2/authorize?client_id=895106609406160978&permissions=8&scope=bot:Suporte,2,5,https\\://discord.gg/};;no]
 `
 }
