1c930cc022ecf1618072185f8501ecb65429b857
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-21 22:19:31 -0300

    Update pause.js

--- Comandos/Music/pause.js
+++ Comandos/Music/pause.js
@@ -3,8 +3,8 @@ name: "pause",
 description: "Pausar uma música!",
 code: `
 $pauseSong
-$reply[$messageID;⏸️ Pausado;yes]
-$onlyIf[$voiceID!=$clientID;<:errado:895110700500934667> » **$username**, você precisa estar no mesmo canal de voz que o bot!]	
-$onlyIf[$queueLength!=0;<:errado:895110700500934667> » **$username**, não possui nenhuma música na queue!]
+$reply[$messageID;Pausado;yes]
+$onlyIf[$voiceID!=$clientID;<:incorrect:909966535769092156> <@$authorID>, você precisa estar no mesmo canal de voz que o bot!]	
+$onlyIf[$queueLength!=0;<:incorrect:909966535769092156> <@$authorID>, não possui nenhuma música na queue!]
 `
 }
