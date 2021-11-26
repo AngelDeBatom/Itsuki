0f9eda8ec1f4504d106b1fb843a8a63a8f58c97f
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-11-26 20:14:50 -0300

    Update queue.js

--- Comandos/Music/queue.js
+++ Comandos/Music/queue.js
@@ -3,12 +3,12 @@ name: "queue",
 aliases: ["q"],
 description: "Ver a lista de música que está tocando!",
 code: `
-$reply[$messageID;
-**Tocando agora:** [$songInfo[title]]($songInfo[url])
-\`\`\`
+<@$authorID>
+$title[**Tocando agora:** [$songInfo[title]]($songInfo[url])]
+$description[\`\`\`
 $queue[1;30;**{number}.** {title} por {username}]
 \`\`\`
-;yes]
+]
 $onlyIf[$voiceID!=$clientID;<:incorrect:909966535769092156> <@$authorID>, você precisa estar no mesmo canal de voz que o bot!]	
 $onlyIf[$queueLength!=0;<:incorrect:909966535769092156> <@$authorID>, não possui nenhuma música na queue!]
 `
