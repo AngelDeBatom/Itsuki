4d7d6798c9da290dd713a42701f08a2448c98cdf
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-12-14 20:56:43 -0300

    Update loop.js

--- Comandos/Music/loop.js
+++ Comandos/Music/loop.js
@@ -3,7 +3,7 @@ name: "loop",
 aliases: ["l"],
 description: "Ativar o loop de uma música",
 code: `
-Loop $reply[$messageID;$replaceText[$replaceText[$loopQueue;true;ativado];false;desativado];yes]
+<@$authorID>, loop $replaceText[$replaceText[$loopQueue;true;ativado];false;desativado]
 $onlyIf[$voiceID!=$clientID;<:incorrect:909966535769092156> <@$authorID>, você precisa estar no mesmo canal de voz que o bot!]	
 $onlyIf[$queueLength!=0;<:incorrect:909966535769092156> <@$authorID>, não possui nenhuma música na queue!]
 `
