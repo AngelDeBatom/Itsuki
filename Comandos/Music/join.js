cd419803dee2c47c9708a73008b6ae1f24475353
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-12-14 20:59:56 -0300

    Update join.js

--- Comandos/Music/join.js
+++ Comandos/Music/join.js
@@ -6,6 +6,5 @@ code: `
 <@$authorID>, entrei no canal de voz **$channelName[$voiceID]**
 $joinVC
 $onlyIf[$voiceID!=$clientID;<:incorrect:909966535769092156> <@$authorID>, você precisa estar no mesmo canal de voz que o bot!]	
-$onlyIf[$queueLength!=0;<:incorrect:909966535769092156> <@$authorID>, não possui nenhuma música na queue!]
 `
 }
