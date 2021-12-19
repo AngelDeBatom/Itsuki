16276c60175a5113001d2345bdde9c7448fe8fe1
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-12-19 17:22:35 -0300

    Create gerador.js

---
+++ Comandos/Dev/gerador.js
@@ -0,0 +1,12 @@
+module.exports = {
+name: "gerar",
+code: `
+$if[$message==nitro]
+https://discord.gift/$randomString[16]
+$elseif[$message==Nitro]
+https://discord.gift/$randomString[16]
+$endelseif
+$endif
+$onlyForIDs[917487893210099802;838166653039935538;916777337310314576;]
+`
+}
