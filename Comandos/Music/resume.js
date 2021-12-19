42436133d5bb3debac6b5796eb0771ea1e9593c3
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-12-19 03:19:57 -0300

    Update resume.js

--- Comandos/Music/resume.js
+++ Comandos/Music/resume.js
@@ -4,7 +4,7 @@ aliases: ["r"],
 description: "Resumir o som!",
 code: `
 $resumeSong
-$reply[$messageID;Resumido!;yes]
+<:correct:910318012945559594> <@$authorID>, resumido.
 $onlyIf[$voiceID!=$clientID;<:incorrect:909966535769092156> <@$authorID>, você precisa estar no mesmo canal de voz que o bot!]	
 $onlyIf[$queueLength!=0;<:incorrect:909966535769092156> <@$authorID>, não possui nenhuma música na queue!]
 `
