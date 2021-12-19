33db51e87c4bbaf4db7959e9321a4ccdf9a3dcb8
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-12-19 12:05:38 -0300

    Update status.js

--- Utils/status.js
+++ Utils/status.js
@@ -1,12 +1,12 @@
 module.exports = (bot) => {
 bot.status({
-text: "$getServerVar[prefix]help ・ dsc.gg/dix ・ $botPingws",
+text: "$getServerVar[prefix]help ・ dsc.gg/dix ・ $pingws",
 type: "LISTENING",
 status: "online",
 time: 12
 })
 bot.status({
-text: "$getServerVar[prefix]help ・ dsc.gg/dix ・ $botPingws",
+text: "$getServerVar[prefix]help ・ dsc.gg/dix ・ $pingws",
 type: "LISTENING",
 status: "online",
 time: 12
