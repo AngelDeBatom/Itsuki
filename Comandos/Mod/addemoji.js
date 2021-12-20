0c0b79e1591885e4ad6635028cb6639d39984422
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-12-20 10:28:56 -0300

    Create addemoji.js

---
+++ Comandos/Mod/addemoji.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "addemoji",
+  aliases: "ad",
+  code: `
+ Emoji $addEmoji[https://cdn.discordapp.com/emojis/$replaceText[$replaceText[$checkCondition[$checkContains[$message[1];<]$checkContains[$message[1];:]$checkContains[$message[1];>]==truetruetrue]$isNumber[$message[1]];truefalse;$replaceText[$advancedTextSplit[$message[1];:;3];>;]];falsetrue;$message[1]];$message[2];yes] adicionado com o nome: **$message[2]**
+ $onlyIf[$charCount[$message[2]]>=2;<:incorrect:909966535769092156> <@$authorID>, você deve colocar um nome mais longo do que **2** caracteres.]
+$onlyIf[$message[2]!=;<:incorrect:909966535769092156> <@$authorID>, tente usar: **$getServerVar[prefix]addemoji <emoji | emojiID> <name>**]
+$onlyPerms[manageemojis;<:incorrect:909966535769092156> <@$authorID>, permissão em falta: **Gerenciar Emojis**]
+$onlyBotPerms[manageemojis;<:incorrect:909966535769092156> <@$authorID>, estou com permissão em falta: **Gerenciar Emojis**.]
+$onlyForChannels[919010522458914846;<@$authorID>, esse canal foi proíbido de se executar meus comandos, tente no canal <#919010522458914846>.]
+$suppressErrors`
+}
