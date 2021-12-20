78f28680e8e94174577e26da0a233f69e4f9049e
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-12-20 10:59:22 -0300

    Update addemoji.js

--- Comandos/Mod/addemoji.js
+++ Comandos/Mod/addemoji.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "addemoji",
   aliases: "ad",
   code: `
-<:incorrect:909966535769092156> <@$authorID>, emoji $addEmoji[https://cdn.discordapp.com/emojis/$replaceText[$replaceText[$checkCondition[$checkContains[$message[1];<]$checkContains[$message[1];:]$checkContains[$message[1];>]==truetruetrue]$isNumber[$message[1]];truefalse;$replaceText[$advancedTextSplit[$message[1];:;3];>;]];falsetrue;$message[1]];$message[2];yes] adicionado com o nome: **$message[2]**
+<:correct:910318012945559594> <@$authorID>, emoji $addEmoji[https://cdn.discordapp.com/emojis/$replaceText[$replaceText[$checkCondition[$checkContains[$message[1];<]$checkContains[$message[1];:]$checkContains[$message[1];>]==truetruetrue]$isNumber[$message[1]];truefalse;$replaceText[$advancedTextSplit[$message[1];:;3];>;]];falsetrue;$message[1]];$message[2];yes] adicionado com o nome: **$message[2]**
 $onlyIf[$charCount[$message[2]]>=2;<:incorrect:909966535769092156> <@$authorID>, você deve colocar um nome mais longo do que **2** caracteres.]
 $onlyIf[$message[2]!=;<:incorrect:909966535769092156> <@$authorID>, tente usar: **$getServerVar[prefix]addemoji <emoji | emojiID> <name>**]
 $onlyPerms[manageemojis;<:incorrect:909966535769092156> <@$authorID>, permissão em falta: **Gerenciar Emojis**]
