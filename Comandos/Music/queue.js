b1be36a0ded90d63d42bd67f0fa1af39491a97a5
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-12-20 16:39:10 -0300

    Update queue.js

--- Comandos/Music/queue.js
+++ Comandos/Music/queue.js
@@ -6,10 +6,20 @@ code: `
 <@$authorID>
 $description[\`\`\`
 Tocando agora: $songInfo[title]
-
-$queue[1;30;{number}. {title} por {username}]
 \`\`\`
 ]
+$addField[Queue 1-30;
+\`\`\`
+$queue[1;30;{number}. {title} por {username}]
+\`\`\`
+;yes]
+$addField[Queue 30-60;
+\`\`\`
+...
+\`\`\`
+;yes]
+
+
 $onlyIf[$voiceID!=$clientID;<:incorrect:909966535769092156> <@$authorID>, você precisa estar no mesmo canal de voz que o bot!]	
 $onlyIf[$queueLength!=0;<:incorrect:909966535769092156> <@$authorID>, não possui nenhuma música na queue!]
 `
