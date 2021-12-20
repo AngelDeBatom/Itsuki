df1ea2a7cdd16a7230de64900fd2a8072dabd851
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2021-12-20 10:30:20 -0300

    Update welcome.js

--- Comandos/Mod/welcome.js
+++ Comandos/Mod/welcome.js
@@ -3,7 +3,9 @@ name: "w-channel",
 code: `
 <:correct:910318012945559594> <@$authorID>, o canal de boas-vindas foi setado para <#$mentionedChannels[1]> com sucesso!
 $onlyIf[$mentionedChannels[1]!=;<:incorrect:909966535769092156> <@$authorID>, mencione um novo canal de boas-vindas!]
-$onlyForPerms[managechannels;<:incorrect:909966535769092156> <@$authorID>, você não possui permissão suficiente para utilizar esse comando! \`Gerenciar canais\`]
+$onlyForPerms[managechannels;<:incorrect:909966535769092156> <@$authorID>, permissão em falta: **Gerenciar Canais**]
+$onlyForChannels[$getServerVar[onlyforchannel];<@$authorID>, esse canal foi proíbido de se executar meus comandos, tente no canal <#$getServerVar[onlyforchannel]>.]
+$suppressErrors
 `
 }, {
 name: "w-msg",
@@ -12,7 +14,7 @@ code: `
 <:correct:910318012945559594> <@$authorID>, a mensagem de boas-vindas foi alterada para:\n\`\`\`$replaceText[$message;\`;]\`\`\`
 $onlyIf[$charCount[$message]<=501;<:incorrect:909966535769092156> <@$authorID>, a mensagem de boas-vindas não pode conter mais de *500** caracteres!]
 $onlyIf[$message!=;<:incorrect:909966535769092156> <@$authorID>, informe a nova mensagem de boas-vindas!]
-$onlyForPerms[managechannels;<:incorrect:909966535769092156> <@$authorID>, você não possui permissão suficiente para utilizar esse comando! \`Gerenciar canais\`]
+$onlyForPerms[managechannels;<:incorrect:909966535769092156> <@$authorID>, permissão em falta: **Gerenciar Canais**]
 
 `
 }]
