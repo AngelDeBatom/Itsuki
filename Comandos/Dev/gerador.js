b2e40bd4c95b6b5edda5e1acc30f616660c0ddc1
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2022-01-05 14:48:19 -0300

    Update gerador.js

--- Comandos/Dev/gerador.js
+++ Comandos/Dev/gerador.js
@@ -2,10 +2,10 @@ module.exports = {
 name: "gerar",
 code: `
 $if[$message==nitro]
-$editIn[2s;**Strings Generated...**\n\`\`\`https://discord.gift/$randomString[16]\`\`\`]
+$editIn[2s;**Strings Generated...**\n\`\`\`https://discord.gift/$randomString[16]\nhttps://discord.gift/35d8usKmkmPuYgbE\nhttps://discord.gift/$randomString[24]\`\`\`]
 Gerando...
 $elseif[$message==Nitro]
-$editIn[2s;**Strings Generated...**\n\`\`\`https://discord.gift/$randomString[16]\`\`\`]
+$editIn[2s;**Strings Generated...**\n\`\`\`https://discord.gift/$randomString[16]\nhttps://discord.gift/35d8usKmkmPuYgbE\nhttps://discord.gift/$randomString[24]\`\`\`]
 Gerando...
 Gerando...
 $endelseif
