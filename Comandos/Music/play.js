1b8f1ba198b115577aba2a4ea5e22cb9e2a19454
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2022-02-10 14:22:55 -0300

    Update play.js

--- Comandos/Music/play.js
+++ Comandos/Music/play.js
@@ -3,8 +3,8 @@ name: "tocar",
 aliases: ["p"],
 description: "Tocar uma música",
 code: `
-<@$authorID>
-<:success:941365484245352498> $playSong[$noMentionMessage;5m;yes;yes;:x: Não foi possível reproduzir a música!] foi adicionado a fila.
+<:success:941365484245352498> <@$authorID>
+**$playSong[$noMentionMessage;5m;yes;yes;:x: Não foi possível reproduzir a música!]** foi adicionado a fila.
 $onlyIf[$message!=;<a:nao:926052919483179008> <@$authorID>, é preciso do nome da música para encontrá-la!]
 $onlyIf[$voiceID!=;<a:nao:926052919483179008> <@$authorID>, você não está em um canal de voz, entre em um!]
 `
