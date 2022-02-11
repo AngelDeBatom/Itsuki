c6c6bc14f42b2e6a231b04a7dbadde059528ebd0
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2022-02-10 23:39:41 -0300

    Update invite.js

--- Comandos/Utilitários/invite.js
+++ Comandos/Utilitários/invite.js
@@ -1,6 +1,6 @@
 module.exports = {
 name: "invite",
 code: `
-$apiMessage[$channelID;<@$authorID>, convide-me para o seu servidor <$, qualquer dúvida ou erro encontrado entre no meu servidor de suporte.;;{actionRow:Convide-me,2,5,https\\://discord.com/api/oauth2/authorize?client_id=$clientID&permissions=8&scope=bot:Suporte,2,5,https\\://discord.gg/wNCr4BTrZv};;no]
+$apiMessage[$channelID;<@$authorID>, invite me to your server, any questions or errors found please join my support server.;;{actionRow:Invite me,2,5,https\\://discord.com/api/oauth2/authorize?client_id=$clientID&permissions=8&scope=bot:Support,2,5,https\\://discord.gg/wNCr4BTrZv};;no]
 `
 }
