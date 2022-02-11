b0382af2de8f4b007351cb853d576d88439ae8a9
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2022-02-10 23:06:28 -0300

    Update play.js

--- Comandos/Music/play.js
+++ Comandos/Music/play.js
@@ -4,15 +4,15 @@ aliases: ["p"],
 description: "Tocar uma música",
 code: `
 <:success:941365484245352498> <@$authorID>
-**$playSong[$noMentionMessage;5m;yes;yes;:x: Não foi possível reproduzir a música!]** foi adicionado a fila.
-$onlyIf[$message!=;<a:nao:926052919483179008> <@$authorID>, é preciso do nome da música para encontrá-la!]
-$onlyIf[$voiceID!=;<a:nao:926052919483179008> <@$authorID>, você não está em um canal de voz, entre em um!]
+**$playSong[$noMentionMessage;5m;yes;yes;:x: could not find the song.]** has been added to the queue.
+$onlyIf[$message!=;<a:nao:926052919483179008> <@$authorID>, you need the name of the song to find it!]
+$onlyIf[$voiceID!=;<a:nao:926052919483179008> <@$authorID>, you're not on a voice channel, join one!]
 `
 }, {
 type: "musicEndCommand",
 channel: "$channelID",
 code: `
-Nenhuma música foi reproduzida nos últimos 5 minutos, saindo. 
+No music played in the last 5 minutes, exiting. 
 `
 }, {
 type: "musicStartCommand",
@@ -20,6 +20,6 @@ channel: "$channelID",
 code: `
 $suppressErrors[]
 $setVar[lastsongplayed;[$songInfo[title]]($songInfo[url])]
-$description[<a:disco:941366579042598962> Tocando agora: [$songInfo[title]]($songInfo[url])]
+$description[<a:disco:941366579042598962> Playing now: [$songInfo[title]]($songInfo[url])]
 `
 }]
