3d6497a7619f527cb07d05e52b2482b3713cffba
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2022-02-10 23:51:38 -0300

    Update disconnect.js

--- Comandos/Music/disconnect.js
+++ Comandos/Music/disconnect.js
@@ -5,7 +5,7 @@ description: "Desconectar o bot de um canal de voz!",
 code: `
 $suppressErrors[]
 $leaveVC
-<a:sim:926052904593412106> <@$authorID>, leaving.
+<:success:941365484245352498> <@$authorID>, leaving.
 $onlyIf[$clientID!=$voiceID;<:incorrect:909966535769092156> <@$authorID>, you need to be connected to the same voice channel as the bot!]	
 $onlyIf[$voiceID!=$clientID;<:incorrect:909966535769092156> <@$authorID>, you need to be connected to the same voice channel as the bot!]	
 `
