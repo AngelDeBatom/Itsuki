a0d5f691a698185f102822ad0c365186ba6d1789
AngelDeBatom <79799244+AngelDeBatom@users.noreply.github.com>
2022-02-10 23:52:58 -0300

    Update join.js

--- Comandos/Music/join.js
+++ Comandos/Music/join.js
@@ -4,7 +4,7 @@ aliases: ["j"],
 description: "Conectar o bot a um canal de voz!",
 code: `
 $suppressErrors[]
-$joinVC
+$joinVC[$voiceID]
 <:success:941365484245352498> <@$authorID>, joing.
 $onlyIf[$voiceID!=$clientID;<:incorrect:909966535769092156> <@$authorID>, you need to be on the same voice channel as the bot!]	
 `
